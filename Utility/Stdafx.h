b66cbc6a48f33d3ccd304acd76cf035615c62a5f
Aaron Sulwer <asulwer@gmail.com>
2022-07-08 18:26:29 -0700

    added C extern code

--- Utility/Stdafx.h
+++ Utility/Stdafx.h
@@ -5,12 +5,14 @@
 #pragma once
 
 #include <stdlib.h>
-//#include <afxwin.h>         // MFC core and standard components
-//#include <afxext.h>         // MFC extensions
-
-#include <process.h>
 
 #define WIN32_LEAN_AND_MEAN
-#pragma managed(push,off) //warning C4793: 'RtlSecureZeroMemory' : function compiled as native
-# include <windows.h>
-#pragma managed(pop)
+//#pragma managed(push,off) //warning C4793: 'RtlSecureZeroMemory' : function compiled as native
+#include <windows.h>
+//#pragma managed(pop)
+
+#ifdef EXPORTS
+    #define DECLDIR "C" __declspec(dllexport)
+#else
+    #define DECLDIR "C" __declspec(dllimport)
+#endif
